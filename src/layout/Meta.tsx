edbbaaa1292e48c9d26472f702909dd59a843c5c
Adnan Syamsafa <137246570+eikarna@users.noreply.github.com>
2024-07-05 19:06:29 +0700

    Update Meta.tsx

--- src/layout/Meta.tsx
+++ src/layout/Meta.tsx
@@ -16,7 +16,7 @@ const Meta = (props: IMetaProps) => {
   return (
     <>
       <Head>
-        <meta charSet="UTF-8" key="charset" />
+        <meta charset="UTF-8" key="charset" />
         <meta
           name="viewport"
           content="width=device-width,initial-scale=1"
@@ -49,6 +49,7 @@ const Meta = (props: IMetaProps) => {
       </Head>
       <NextSeo
         title={props.title}
+        keyword={props.keyword}
         description={props.description}
         canonical={props.canonical}
         openGraph={{
