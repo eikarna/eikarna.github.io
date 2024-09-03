2154d966296ddb5e819d66ccf4cf057dbe703597
Adnan Syamsafa <137246570+eikarna@users.noreply.github.com>
2024-09-04 06:43:05 +0700

    Update AppConfig.ts

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,7 +1,7 @@
 export const AppConfig = {
   site_name: 'Eikarna',
   title: 'Eikarna Portfolio',
-  description: "Eikarna adalah seorang Programmer Independen dari Indonesia. Dia menyukai Beberapa topik seperti Artificial Intelligence (AI), Modding, dan Machine Learning (ML).",
+  description: "Eikarna atau lebih dikenal sebagai Adnan adalah seorang Programmer Independen dari Indonesia. Dia menyukai Beberapa topik seperti Artificial Intelligence (AI), Modding, dan Machine Learning (ML).",
   locale: 'id',
   keyword: 'Adnan Syamsafa, Adnan, Eikarna, Portofolio, Eikarna Portofolio, PhynX404, phynx404, Nixic404, Nixic0, nixic404, nixic0, adnan syamsafa, adnan, eikarna, portofolio, eikarna portofolio, programmer indonesia, programmer junior',
 };
